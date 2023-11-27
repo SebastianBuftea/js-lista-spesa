cd037c356ac03a9ecc605848a4c9c1152f3317ca
SebastianBuftea <sebibuftea85@gmail.com>
2023-11-27 15:36:04 +0100

    [UPDATE] FINISH

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+// creiamo la lista della spesa
+const lista=["guanciale", "uova", "pasta", "pepe", "pecorino"];
+
+// dopo aver inizializzato il contatore a zero  eseguiamo la stampa degli elementi della lista finche non arriveremo al ultimo
+let i=0;
+while (i < lista.length){
+    console.log(lista[i]);
+    i++;
+}
